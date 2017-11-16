d6552f7327d427855842ac93307a5909f8552404
jurepustos <jure.poglednik@gmail.com>
2017-11-16 21:59:53 +0100

    should not crash now

--- app.js
+++ app.js
@@ -2,3 +2,15 @@ var koa = require('koa');
 var bodyParser = require('koa-bodyParser');
 
 var app = koa();
+
+app.use(bodyParser());
+
+app.use(async ctx => {
+  // the parsed body will store in ctx.request.body
+  // if nothing was parsed, body will be an empty object {}
+  ctx.body = ctx.request.body;
+});
+
+
+
+app.listen(80);
