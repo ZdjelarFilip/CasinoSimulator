21c6bd21aaa0d8ab059f0fa1fd6bfa897aa52bba
jurepustos <jure.poglednik@gmail.com>
2017-11-19 22:40:04 +0100

    installed a bunch of modules

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var Koa = require('koa');
-var bodyParser = require('koa-bodyparser');
+const Koa = require('koa');
+const bodyParser = require('koa-body');
+const cookies = require('koa-cookie');
 
 var app = new Koa();
 
 app.use(bodyParser());
+app.use(cookies());
 
-app.use(async ctx => {
+app.use(async (ctx, next) => {
   // the parsed body will store in ctx.request.body
   // if nothing was parsed, body will be an empty object {}
   ctx.body = ctx.request.body;
+  //await next();
 });
 
 
