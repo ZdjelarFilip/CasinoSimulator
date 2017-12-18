7c8711c9648fc88c73e153877facb0acc2d6d3ec
jurepustos <jure.poglednik@gmail.com>
2017-12-18 22:36:00 +0100

    let's say that I did something today

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(async (ctx, next) => {
 	// the parsed body will store in ctx.request.body
 	// if nothing was parsed, body will be an empty object {}
 	ctx.body = ctx.request.body
-	//await next();
+	await next();
 });
 
 
--- router.js
+++ router.js
@@ -1,3 +1,9 @@
-const Router = require('koa-router');
+import Router from 'koa-router';
+import render from 'koa-ejs';
 
 var router = new Router();
+
+
+router.get('/', (ctx, next) => {
+	
+});
